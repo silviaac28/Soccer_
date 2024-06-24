cf575b4dd6e55bc3e393a6cdc52974fb4bfd3092
silviaac28 <silviaangarita28@gmail.com>
2024-06-23 23:30:34 -0500

    first commit

---
+++ src/main/java/com/soccer/Main.java
@@ -0,0 +1,8 @@
+package com.soccer;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controlador = new Controller();
+        controlador.iniciarViewTeam();
+    }
+}
